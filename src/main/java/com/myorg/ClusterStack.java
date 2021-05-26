8d663c65fcca3782fc717b2f553822158c2bd835
NylRJ <loveloungeoficial@gmail.com>
2021-05-26 15:21:46 -0300

    Createting the Cluster

--- src/main/java/com/myorg/ClusterStack.java
+++ src/main/java/com/myorg/ClusterStack.java
@@ -4,12 +4,12 @@ import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
 
-public class CursoAwsCdkStack extends Stack {
-    public CursoAwsCdkStack(final Construct scope, final String id) {
+public class ClusterStack extends Stack {
+    public ClusterStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
 
-    public CursoAwsCdkStack(final Construct scope, final String id, final StackProps props) {
+    public ClusterStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
         // The code that defines your stack goes here
